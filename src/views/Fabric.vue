2b3d7b5bd3e36ec00dabb90f06cc0c36b1466ead
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-09-10 14:44:11 +0900

    eslint 노란줄 해결

--- src/views/Fabric.vue
+++ src/views/Fabric.vue
@@ -1,53 +1,53 @@
 <template>
-    <div>
-        <div>
-            <input type="number" v-model.number="numberModel"><br>
-            <button id="add" onclick="Add()">Add a rectangle</button>
-        </div>
+  <div>
+    <!-- 이미지 업로드 버튼 -->
+    <div><input id="uploader" type="file" /><br /></div>
 
-        <canvas ref="can" width="800" height="800" style="border:1px solid #ccc"></canvas>
-
-    </div>
+    <canvas ref="can" id="canvas" width="1000" height="1000"></canvas>
+  </div>
 </template>
 <script>
-    import {
-        fabric
-    } from 'fabric';
+import { fabric } from "fabric";
 
-    export default {
-        name: '',
-        components: {},
-        data() {
-            return {
-                numberModel: 3
-            };
-        },
-        created() {},
-        mounted() {
-            const ref = this.$refs.can;
-            const canvas = new fabric.Canvas(ref);
-            const rect1 = new fabric.Rect({
-                fill: 'red',
-                width: 200,
-                height: 200
-            });
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      numberModel: 3,
+    };
+  },
+  created() {},
+  mounted() {
+    const ref = this.$refs.can;
+    const canvas = new fabric.Canvas(ref);
+    const rect1 = new fabric.Rect({
+      fill: "red",
+      width: 200,
+      height: 200,
+    });
 
-            const rect2 = new fabric.Rect({
-                fill: 'blue',
-                width: 300,
-                height: 300
-            });
+    const rect2 = new fabric.Rect({
+      fill: "blue",
+      width: 300,
+      height: 300,
+    });
 
-            const cir1 = new fabric.Rect({
-                fill: "green",
-                width: 300,
-                height: 300,
-            })
+    const cir1 = new fabric.Rect({
+      fill: "green",
+      width: 300,
+      height: 300,
+    });
 
-            canvas.add(rect1).add(rect2).add(cir1);
-        },
-        updated() {},
-        unmounted() {},
-        methods: {}
-    }
+    canvas.add(rect1).add(rect2).add(cir1);
+  },
+  updated() {},
+  unmounted() {},
+  methods: {},
+};
 </script>
+<style scoped>
+canvas {
+  border: 5px solid black;
+}
+</style>

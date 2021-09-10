6c86ce3e71172cc7f06d6eed7262d4e0fd521cfb
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-09-10 14:23:58 +0900

    처음

---
+++ src/views/Fabric.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div>
+            <input type="number" v-model.number="numberModel"><br>
+            <button id="add" onclick="Add()">Add a rectangle</button>
+        </div>
+
+        <canvas ref="can" width="800" height="800" style="border:1px solid #ccc"></canvas>
+
+    </div>
+</template>
+<script>
+    import {
+        fabric
+    } from 'fabric';
+
+    export default {
+        name: '',
+        components: {},
+        data() {
+            return {
+                numberModel: 3
+            };
+        },
+        created() {},
+        mounted() {
+            const ref = this.$refs.can;
+            const canvas = new fabric.Canvas(ref);
+            const rect1 = new fabric.Rect({
+                fill: 'red',
+                width: 200,
+                height: 200
+            });
+
+            const rect2 = new fabric.Rect({
+                fill: 'blue',
+                width: 300,
+                height: 300
+            });
+
+            const cir1 = new fabric.Rect({
+                fill: "green",
+                width: 300,
+                height: 300,
+            })
+
+            canvas.add(rect1).add(rect2).add(cir1);
+        },
+        updated() {},
+        unmounted() {},
+        methods: {}
+    }
+</script>

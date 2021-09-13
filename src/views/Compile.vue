62c6cdd822835ebe69c200a33f33bf34a449c360
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-09-13 10:58:26 +0900

    compile 중간 세이브포인트

--- src/views/Compile.vue
+++ src/views/Compile.vue
@@ -1,19 +1,35 @@
 <template>
-    <div></div>
+    <div>
+        <textarea name="" id="" cols="30" rows="10" v-model="message">
+
+        </textarea>
+        <p v-for="(a,i) in line" :key="a">{{i}} : {{a}}</p>
+
+
+    </div>
 </template>
 <script>
-export default {
-    name: '',
-    components: {},
-    data () {
-        return {
-            sampleData: ''
+    export default {
+        name: '',
+        components: {},
+        data() {
+            return {
+                line: ["pragma solidity 0.8.6",
+                    "contract Test2{",
+                    "string name;",
+                    "function changeName (string Memory _ name) public {",
+                    "name = _name;}",
+                    "function viewName () public view returns (string memory){",
+                    "return name;}"
+                ]
+            }
+        },
+        created() {},
+        mounted() {},
+        updated() {},
+        unmounted() {},
+        methods: {
+
         }
-    },
-    created () {},
-    mounted () {},
-    updated () {},
-    unmounted () {},
-    methods: {}
-}
+    }
 </script>

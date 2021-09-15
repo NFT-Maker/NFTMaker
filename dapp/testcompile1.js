cd992d37f342be4d08e727d5e12eb86251aad6d6
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-09-15 14:01:31 +0900

    중간 세이브

--- dapp/testcompile1.js
+++ dapp/testcompile1.js
@@ -4,14 +4,14 @@ const fileSystem = require("fs-extra");
 
 
 //Preparing for build folder
-const exportPath = path.resolve(__dirname, "build");
-fileSystem.removeSync(exportPath);
+const buildPath = path.resolve(__dirname, "build");
+fileSystem.removeSync(buildPath);
 
 //Get contract path
-const testDoContract = path.resolve(__dirname, "contracts", "testDo.sol");
+const contractsPath = path.resolve(__dirname, "contracts", "testDo.sol");
 
 //Read the contract from testDo path
-const testDoSource = fileSystem.readFileSync(testDoContract, "utf8");
+const testDoSource = fileSystem.readFileSync(contractsPath, "utf8");
 
 var input = {
     language: 'Solidity',
@@ -34,12 +34,12 @@ try{
 
     for (let contract in output.contracts["testDo.sol"]) {
         fileSystem.outputJSONSync(
-          path.resolve(exportPath, "testDoABI.json"),
+          path.resolve(buildPath, "testDoABI.json"),
           output.contracts["testDo.sol"][contract].abi
         );
     
         fileSystem.outputJSONSync(
-          path.resolve(exportPath, "testDoBytecode.json"),
+          path.resolve(buildPath, "testDoBytecode.json"),
           output.contracts["testDo.sol"][contract].evm.bytecode.object
         );
       }

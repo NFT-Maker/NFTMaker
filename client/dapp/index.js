e43e185a9e72bdf52c9c397cf0dd1ef9829ecb6d
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-09-24 05:09:50 +0900

    Update index.js

--- client/dapp/index.js
+++ client/dapp/index.js
@@ -1,7 +1,3 @@
 module.exports.Payments = require("./build/Payments");
 module.exports.testDo = require("./build/testDo");
 module.exports.$swal = require("sweetalert2");
-<script
-  type="text/javascript"
-  src="https://solc-bin.ethereum.org/bin/list.js"
-></script>

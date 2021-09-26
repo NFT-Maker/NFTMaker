76aa329d6b7030b0b223a1c7f9b4a5c7b67bba09
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-09-27 07:13:16 +0900

    이미지

--- client/src/views/HowToImg.vue
+++ client/src/views/HowToImg.vue
@@ -1,96 +1,96 @@
 <template>
     <div>
         <Nav />
-        <b-card
-            class="mx-2 my-2"
-            header-bg-variant="warning"
-            header-text-variant="dark"
-        >
-            <!-- 헤더 -->
-            <template #header>
-                <h6 class="mb-0">NFT 발행 방법 선택</h6>
-            </template>
-            <div class="bg-light container-fluid" style="height: 75vh">
-                <!-- 자신만의 컨트랙트를 만드는 화면 -->
-                <br />
-                <br />
-                <br />
-                <div class="d-flex justify-content-center">
-                    <b-button variant="dark" @click="setting(0)"
-                        >이미지 편집 + 컨트랙트 편집</b-button
-                    >
-                </div>
-                <br />
-                <br />
-                <div class="d-flex justify-content-center">
-                    <b-button variant="dark" @click="setting(1)"
-                        >이미지 편집(NFT Maker)</b-button
-                    >
-                </div>
-                <!-- NFT Maker의 컨트랙트로 NFT 만드는 화면 -->
-                <br /><br />
 
-                <div class="input-group mb-3">
-                    <input
-                        type="text"
-                        v-model="text"
-                        class="form-control"
-                        placeholder="기존 컨트랙트 주소 입력(12입력하고 버튼클릭하면 이동가능)"
-                        aria-label="기존 컨트랙트 주소 입력"
-                        aria-describedby="basic-addon2"
-                    />
-                    <div class="input-group-append">
-                        <b-button variant="dark" @click="alreadyContract()">
-                            이미지 편집 + 기존 컨트랙트
-                        </b-button>
-                    </div>
-                </div>
-            </div>
-        </b-card>
+        <b-card-group deck class="mx-auto my-3 col-10" nobody>
+            <b-card title="NFT 발행" img-src="../assets/img/화면/nft01.png" img-alt="Image" img-top>
+
+                <b-card-text style="height: 80px">
+                   컨트렉트 발행없이 NFT를 발행할 수 있습니다.
+                </b-card-text>
+             
+                <b-button variant="dark" block  @click="setting(1)">이동</b-button>
+            </b-card>
+
+
+            <b-card title="NFT 및 NFT 컨트렉트 발행" img-src="../assets/img/화면/nft02.png" img-alt="Image" img-top>
+
+                <b-card-text style="height: 80px">
+                   자신만의 컨트렉트를 만들고 컨트렉트 및 NFT를 배포할 수 있습니다.
+                </b-card-text>
+
+                <b-button variant="dark" block @click="setting(0)">NFT 및 NFT 컨트렉트 발행하기</b-button>
+            </b-card>
+
+
+
+
+
+            <b-card title="배포된 컨트렉트로 NFT 발행" img-src="../assets/img/화면/nft03.png" img-alt="Image" img-top>
+
+                <b-card-text>
+                    배포된 컨트렉트 주소를 통해 NFT를 발행할 수 있습니다.
+                </b-card-text>
+
+                <b-form-input type="text" v-model="text" class="form-control" placeholder="컨트렉트 주소 입력"
+                    aria-label="기존 컨트랙트 주소 입력" aria-describedby="basic-addon2"></b-form-input>
+
+
+                <b-button variant="dark" block @click="alreadyContract()">
+                    배포된 컨트렉트로 NFT 발행하기
+                </b-button>
+
+            </b-card>
+
+        </b-card-group>
+
     </div>
 </template>
 <script>
-import Nav from "../components/Nav123.vue";
-import $Swal from "sweetalert2";
-export default {
-    name: "",
-    components: {
-        Nav,
-    },
-    data() {
-        return {
-            text: "",
-        };
-    },
-    setup() {},
-    created() {},
-    mounted() {},
-    unmounted() {},
-    methods: {
-        alreadyContract() {
-            this.$store.commit("contractAccountSave", this.text);
-            console.log(this.$store.state.contractAccount);
-            this.$api("/api/list", "post", {
-                param: [this.$store.state.contractAccount],
-            }).then((result) => {
-                console.log(result);
-                if (result.length == 0) {
-                    new $Swal(
-                        "'NFT Maker'에서 발행한 컨트랙트 주소가 아닙니다"
-                    );
-                } else if (result[0].EOA != this.$store.state.account) {
-                    new $Swal("당신은 이 컨트랙트의 주인이 아닙니다");
-                } else {
-                    this.$store.commit("abiSave", result[0].CA_abi);
-                    console.log(this.$store.state.abi);
-                    this.setting(2);
-                }
-            });
+    import Nav from "../components/Nav123.vue";
+    import $Swal from "sweetalert2";
+
+    export default {
+        name: "",
+        components: {
+            Nav,
         },
-        setting(num) {
-            this.$store.commit("settingNumSave", num);
-            this.$router.push({ path: "imageBasic" });
+        data() {
+            return {
+                text: "",
+            };
         },
-    },
-};
+        setup() {},
+        created() {},
+        mounted() {},
+        unmounted() {},
+        methods: {
+            alreadyContract() {
+                this.$store.commit("contractAccountSave", this.text);
+                console.log(this.$store.state.contractAccount);
+                this.$api("/api/list", "post", {
+                    param: [this.$store.state.contractAccount],
+                }).then((result) => {
+                    console.log(result);
+                    if (result.length == 0) {
+                        new $Swal(
+                            "'NFT Maker'에서 발행한 컨트랙트 주소가 아닙니다"
+                        );
+                    } else if (result[0].EOA != this.$store.state.account) {
+                        new $Swal("당신은 이 컨트랙트의 주인이 아닙니다");
+                    } else {
+                        this.$store.commit("abiSave", result[0].CA_abi);
+                        console.log(this.$store.state.abi);
+                        this.setting(2);
+                    }
+                });
+            },
+            setting(num) {
+                this.$store.commit("settingNumSave", num);
+                this.$router.push({
+                    path: "imageBasic"
+                });
+            },
+        },
+    };
 </script>
